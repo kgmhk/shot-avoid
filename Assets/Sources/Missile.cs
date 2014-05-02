a5321447e93f0af0a096f808920d286925831d85
gihyun <gihyunkwak@naver.com>
2014-05-02 10:08:18 +0900

    2014-05-02 벽 충돌 테스트중..

---
+++ Assets/Sources/Missile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour {
+
+	private float xPos = 0.0f;
+	private float yPos = 0.0f;
+	private int check = 0;
+	private int rNumber = Random.Range(-1,1);
+	void Start () {
+		this.rigidbody2D.velocity = new Vector2(rNumber, 5.0f);
+	}
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		xPos = this.transform.position.x;
+		yPos = this.transform.position.y;
+
+		if (xPos < -2.2) {
+			this.rigidbody2D.velocity = new Vector2 (1, 0);
+			print ("vector" + this.transform.position.x);
+		} 
+		else if(xPos > 2.2){
+			this.rigidbody2D.velocity = new Vector2 (-1, 0);
+		}
+
+
+	}
+	
+
+}
